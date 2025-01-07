093766f49740c21571cda8d793940037b4b42f45
zouak-pedro <zouakpedro@gmail.com>
2025-01-07 12:18:34 +0100

    link the api data with the html and get the city name from the input to start the api request

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const apiKey = "24a83226bea88ea935fbce437b78b6eb";
 let cityName = "annaba";
-let apiData;
+let apiData = {};
+apiData.name = "";
+
 function checkweather(city) {
   if (!city || typeof city !== "string" || city.trim() === "") {
     console.error("Invalid city name. Please provide a valid city name.");
+  } else if (apiData.name === city) {
+    console.error("You have already searched.");
   } else {
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
@@ -17,6 +21,7 @@ function checkweather(city) {
       .then((data) => {
         apiData = data;
         console.log(data);
+        updatePage(apiData);
       })
       .catch((error) => {
         console.error("Problem with fetch operation", error);
@@ -25,3 +30,36 @@ function checkweather(city) {
 }
 
 // checkweather(cityName);
+
+document.querySelector(".search_bar_search").addEventListener("click", () => {
+  cityName = document.querySelector(".search_bar").value;
+  checkweather(cityName);
+});
+
+document.addEventListener("keydown", function (event) {
+  let keyCode = event.keyCode;
+  if (keyCode === 13) {
+    cityName = document.querySelector(".search_bar").value;
+    checkweather(cityName);
+  }
+});
+
+function updatePage(data) {
+  document.querySelector(".search_location_info_name").innerHTML = apiData.name;
+  document.querySelector(".search_location_info_fullname").innerHTML =
+    apiData.name + ", " + apiData.sys.country;
+  document.querySelector(".main_temp_data").innerHTML = Math.round(
+    apiData.main.temp
+  );
+  document.querySelector(".detail_weather_state").innerHTML =
+    apiData.weather[0].description;
+  document.querySelector(".detail_weather_humidity").innerHTML =
+    apiData.main.humidity;
+  document.querySelector(
+    ".main_dayimg"
+  ).style.backgroundImage = `url(https://openweathermap.org/img/wn/${apiData.weather[0].icon}@4x.png)`;
+
+  document.querySelector(".detail_weather_wind").innerHTML = Math.round(
+    apiData.wind.speed
+  );
+}
